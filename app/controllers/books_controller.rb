f479707775b1f4190eda4954a145ee92613f9f7c
Kent Broestl <kbroestl@innova-partners.com>
2010-04-29 13:55:51 -0400

    adding the pagination plugin and a bit on the book index which makes it work.g

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,11 +2,13 @@ class BooksController < ApplicationController
   # GET /books
   # GET /books.xml
   def index
-    @books = Book.all
-
+#    @books = Book.all
+    @books = Book.paginate :page=>params[:page]
+    
     respond_to do |format|
       format.html # index.html.erb
       format.xml  { render :xml => @books }
+      format.json { render :json => @books }
     end
   end
 
