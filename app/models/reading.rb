179f68cab2996cfb60074bf08c8242fb96370e23
Kent Broestl <kbroestl@innova-partners.com>
2014-12-30 11:41:36 -0500

    More migration cleanup.

--- app/models/reading.rb
+++ app/models/reading.rb
@@ -2,6 +2,4 @@ class Reading < ActiveRecord::Base
   belongs_to :book
   
   attr_protected :book_id
-  
-  
 end
--- db/migrate/20110419210447_add_timestamps_to_books.rb
+++ db/migrate/20110419210447_add_timestamps_to_books.rb
@@ -2,11 +2,9 @@ class AddTimestampsToBooks < ActiveRecord::Migration
   def self.up
     add_column :books, :created_at, :timestamp
     add_column :books, :updated_at, :timestamp
-    remove_column :books, :last_modified
   end
 
   def self.down
-    add_column :books, :last_modified, :timestamp
     remove_column :books, :updated_at
     remove_column :books, :created_at
   end
