ef2d30b1d3a0cae5107c44c2650cb1a650e6135c
Kent Broestl <kbroestl@innova-partners.com>
2015-01-02 16:13:07 -0500

    Updated tests. Starting to test custom methods on the models.

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -4,4 +4,9 @@ RSpec.describe Author, :type => :model do
   it "has a valid factory" do
     expect(FactoryGirl.create(:author)).to be_valid
   end
+
+  it "returns an author's full name as a string" do
+    author = FactoryGirl.create(:author, author_first: "Isaac", author_last: "Asimov")
+    expect(author.name).to eq("Isaac Asimov")
+  end
 end
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -4,4 +4,14 @@ RSpec.describe Book, :type => :model do
   it "has a valid factory" do
     expect(FactoryGirl.create(:book)).to be_valid
   end
+
+  it "returns sortable title" do
+    book = FactoryGirl.create(:book, title: "The Rise and Fall of the Roman Empire")
+    expect(book.sortby_title).to eq("Rise and Fall of the Roman Empire")
+  end
+
+  it "returns sortable German title" do
+    book = FactoryGirl.create(:book, title: "Des Teufels General")
+    expect(book.sortby_title).to eq("Teufels General")
+  end
 end
