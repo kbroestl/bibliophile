83d4254a03e13373955c1c2896eab648f78827b5
Kent Broestl <kbroestl@innova-partners.com>
2019-12-10 15:10:22 -0500

    First stab at using rails app:update

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@
 source 'https://rubygems.org'
 
 gem 'rails', '5.2.3'
+gem 'bootsnap'
 
 gem 'rake'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,3 @@
-# frozen_string_literal: true
-
-RailsPortal::Application.routes.draw do
-  resources :books do
-    resources :readings
-  end
-  resources :authors
-  resources :locations
-  resources :genres
-  resources :authorships
-  resources :languages
-
-  root to: 'books#index'
-
-  match 'unread' => 'books#unread', :via => 'get'
-
-  match 'lchart' => 'languages#languageschart', :via => 'get'
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
