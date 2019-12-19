846a35939d0afaa969f207ea4515a0da37e0af79
Kent Broestl <kbroestl@innova-partners.com>
2019-12-19 10:25:38 -0500

    Further trying to sort out the name attribute. No massive progress yet.

--- spec/factories/languages.rb
+++ spec/factories/languages.rb
@@ -3,7 +3,7 @@
 require 'faker'
 
 FactoryBot.define do
-  factory :language do
-    name { Faker::Lorem.words(number: 1) }
+  factory :language, class: 'Language' do |l|
+    l.name { Faker::Lorem.words(number: 1) }
   end
 end
