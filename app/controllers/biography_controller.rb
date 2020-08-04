e4eebdb71df0a2383d3a42d29b4d49f0fb4862e0
Kent Broestl <kbroestl@gmail.com>
2020-08-03 21:07:44 -0400

    working on saving the bios locally, to later not spam wikipedia with requests for stale data.

--- app/controllers/biography_controller.rb
+++ app/controllers/biography_controller.rb
@@ -1,2 +1,82 @@
+# frozen_string_literal: true
+
 class BiographyController < ApplicationController
+  # GET /biographies is not a typical use case for this controller 
+
+  # GET /biographies/1
+  # GET /biographies/1.xml
+  def show
+    @bio = Biography.find(params[:id])
+    @author = Author.find(@bio.author_id)
+    @page_tite = 'Details for #{@author.name}'
+
+    respond_to do |format|
+      format.html
+      format.xml { render xml: @bio }
+    end
+  end
+
+  # GET /biographies/new is not a typical use case for this controller
+  # biographies will be populated with content from wikipedia, confirmed by
+  # user input as needed.
+  #
+  #GET /biographies/1/edit
+  def edit
+    @bio = Biography.find(params[:id])
+  end
+
+  # POST /biographies
+  # POST /biographies.xml
+  def create
+    @bio = Biography.new(biography_params)
+
+    respond_to do |format|
+      if @bio.save
+        flash[:notice] = 'Biography was succefully created'
+        format.html { redirect_to(@bio) }
+        format.xml { render xml: @bio, status: :created, location: @bio }
+      else
+        flash[:notice] = "Something's gone horribly wrong"
+        format.html { render action: 'new' }
+        format.xml  { render xml: @bio.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /biographies/1
+  # PUT /biographies/1.xml
+  def update
+    @bio = Biography.find(params[:id])
+
+    respond_to do |format|
+      if @bio.update_attributes(biography_params)
+        flash[:notice] = 'Biography was successfully updated.'
+        format.html { redirect_to(@author) }
+        format.xml  { head :ok }
+      else
+        format.html { render action: 'edit' }
+        format.xml  { render xml: @author.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+  
+  # DELETE /biographies/1
+  # DELETE /biographies/1.xml
+  def destroy
+    @author = Author.find(params[:id])
+    @author.destroy
+
+    respond_to do |format|
+      format.html { redirect_to(authors_url) }
+      format.xml  { head :ok }
+    end
+  end
+
+  private
+
+  def biography_params
+    params.require(:biography).permit(
+      :image_url
+    )
+  end
 end
--- app/models/biography.rb
+++ app/models/biography.rb
@@ -1,15 +1,26 @@
+# frozen_string_literal: true
+
 require 'wikipedia'
 
 class Biography < ApplicationRecord
-	belongs_to :author
+  belongs_to :author
 
-	def self.fetch_bio (author)
-	  bio = Wikipedia.find( author.fullname )	
-	end
-	# rudimentary version works. Fetches by author name.
-	# Need to save bio.summary, bio.image_url to database
-	# Additionally, check database for last fetch, as to not spam the wikipedia
-	# servers.
-	#
-	# From author view, fetch info into sidebar, may need ajax later.
+  def self.fetch_bio (author)
+    bio = Wikipedia.find( author.fullname )
+
+    # self.image_url = bio.main_image_url
+    # self.bio_text = bio.summary
+    # self.author_id = author.id
+  end
+  # rudimentary version works. Fetches by author name.
+  # Need to save bio.summary, bio.image_url to database
+  # Additionally, check database for last fetch, as to not spam the wikipedia
+  # servers.
+  #
+  # From author view, fetch info into sidebar, may need ajax later.
+
+  # Wikipedia API does not allow for pulling content via URL, only search by
+  # title, which might match 100%. Need to have a way to circumvent that 
+  # system: Maybe allow to override the title (@author.full_name, by default)
+  # with a user-confirmed value.
 end
