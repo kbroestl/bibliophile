42f58436fc560ec4652c320852f6d7af0bf022bc
Kent Broestl <kbroestl@gmail.com>
2025-12-31 20:03:21 -0500

    just updating the files before resolving some dependabot flagged isues

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,7 +1,15 @@
 # frozen_string_literal: true
 
-# Basic Active Record class.
-# No business logic here as yet.
 class Location < ApplicationRecord
   has_many :books
+
+  def self.count_by_location
+    query = <<-SQL
+    select count(b.location_id) as "total", l.name, l.id
+    from locations l
+    Inner Join books b on b.location_id = l.id
+    group by l.id
+    SQL
+    Location.find_by_sql(query)
+  end
 end
