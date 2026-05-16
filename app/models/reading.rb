9ce1ab71f6af6b1716c5b07a04b9574a066a6164
Kent Broestl <kbroestl@gmail.com>
2026-05-16 14:04:40 -0400

    Has a working scope to pull books which would have been in progress given a date range. Now to figure out how to put it into the interface, controllers, etc.

--- app/models/reading.rb
+++ app/models/reading.rb
@@ -3,4 +3,17 @@
 # Basic ActiveRecord model Nothing fancy.
 class Reading < ApplicationRecord
   belongs_to :book
+
+  scope :overlapping, ->(start_date, end_date) {
+    where(
+      "date_started <= :end_date
+        AND (
+        date_finished IS NULL
+        OR
+        date_finished >= :start_date
+        )",
+        start_date: start_date,
+        end_date: end_date
+    )
+  }
 end
